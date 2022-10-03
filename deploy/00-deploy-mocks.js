ae2d5ee90bbcc20a1c217d3956c4585f74a31999
simsim <simeon.campbell@live.com>
2022-10-03 13:52:09 +0700

    Initial commit

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,26 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+
+const BASE_FEE = ethers.utils.parseEther("0.25") // Premium = 0.25LINK per call
+const GAS_PRICE_LINK = 1e9 // based on gas price of the chain - link per gas.
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const cainId = network.config.chainId
+    const args = [BASE_FEE, GAS_PRICE_LINK]
+
+    if (developmentChains.includes(network.name)) {
+        log("Local network detected! Deploying mocks...")
+        // deploy mock vrfcoordinator
+        await deploy("VRFCoordinatorV2Mock.sol", {
+            from: deployer,
+            log: true,
+            args: args,
+        })
+        log("Mocks Deployed!")
+        log("--------------------------------------------------")
+    }
+}
+
+module.exports.tags = ["all", "mocks"]
---
+++ deploy/01-deploy-lottery.js
@@ -0,0 +1,53 @@
+const { network, ethers, getChainId } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../helper-hardhat-config")
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("30")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address, subscriptionId, VRFCoordinatorV2Mock
+
+    if (developmentChains.includes(network.name)) {
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address
+        const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
+        const transactionReciept = await transactionResponse.wait(1)
+        subscriptionId = transactionReciept.events[0].args.subId
+
+        await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
+    }
+
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    const interval = networkConfig[chainId]["interval"]
+    console.log(vrfCoordinatorV2Address)
+    const args = [
+        vrfCoordinatorV2Address,
+        entranceFee,
+        gasLane,
+        subscriptionId,
+        callbackGasLimit,
+        interval,
+    ] // <---------got to 3rd arg (15:17:10 in video)
+    const lottery = await deploy("lottery", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(lottery.address, args)
+    }
+    log("-----------------------------------------------")
+}
+
+module.exports.tags = ["all", "lottery"]
